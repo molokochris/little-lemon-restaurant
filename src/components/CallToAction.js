9a4197695dd8257ae545885ff692a0216088ee83
molokochris <molokochrisp742@gmail.com>
2026-04-09 13:44:44 +0200

    same as built project

--- src/components/CallToAction.js
+++ src/components/CallToAction.js
@@ -3,21 +3,20 @@ import { Link } from "react-router-dom";
 const CallToAction = () => {
   return (
     <section id="hero">
-      <article>
+      <article className="hero-text">
         <h1>Little Lemon</h1>
-        <h2>Chicago</h2>
-        <p>
-          We are a family-owned Mediterranean restaurant, focused on traditional
-          recipes served with a modern twist.{" "}
-        </p>
-        <button className="button" role="button" aria-label="Reserve a Table">
-          <Link to="/booking">Reserve a Table</Link>
-        </button>
+        <h2 style={{ color: "rgb(237, 239, 238)" }}>Chicago</h2>
+        <p style={{ color: "rgb(237, 239, 238)" }}>
+          We are a family-owned Mediterranean restaurant, focused on traditional recipes served with
+          a modern twist.</p>
+        <br />
+        <a className="button" role="button" aria-label="Reserve a Table" href="#/booking" data-discover="true" style={{ display: "inline-block", marginTop: "1rem" }}>Reserve a Table</a>
       </article>
-      <img
-        src={require("../images/restauranfood.jpg")}
-        alt="A table with a variety of Mediterranean dishes"
-      />
+      <div className="hero-image-container">
+        <img alt="A table with a variety of Mediterranean dishes"
+          src={require("../images/restauranfood.jpg")}
+        />
+      </div>
     </section>
   );
 };
--- src/components/Chicago.js
+++ src/components/Chicago.js
@@ -1,19 +1,23 @@
 const Chicago = () => {
   return (
     <section id="about">
-      <article>
+      <article className="about-text">
         <h1>Little Lemon</h1>
         <h2>Chicago</h2>
-        <p>
-          Little Lemon is a family owned Mediterranean restaurant located in the
-          heart of Chicago. We are passionate about serving traditional recipes
-          with a modern twist, using fresh and locally sourced ingredients. Our
-          mission is to provide our customers with an unforgettable dining
-          experience that combines delicious food, warm hospitality, and a cozy
-          atmosphere.
-        </p>
+        <p>Little Lemon is a family owned Mediterranean
+          restaurant located in the heart of Chicago.
+          We are passionate about serving traditional recipes
+          with a modern twist, using fresh and locally sourced
+          ingredients. Our mission is to provide our customers
+          with an unforgettable dining experience that combines
+          delicious food, warm hospitality, and a cozy atmosphere.</p>
       </article>
-      <img src={require("../images/restauranfood.jpg")} alt="Restaurant Food" />
+      <div className="about-images">
+        <img alt="Restaurant Food" className="about-img-1"
+          src={require("../images/restauranfood.jpg")} />
+        <img alt="Greek Salad" className="about-img-2"
+          src={require("../images/greek salad.jpg")} />
+      </div>
     </section>
   );
 };
